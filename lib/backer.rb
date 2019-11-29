7388233f11e38e0290a2f83681d4714d01f9b0ed
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 03:53:27 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,13 @@
 class Backer
-  
-  
-  
+  attr_reader :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+  end
 end
